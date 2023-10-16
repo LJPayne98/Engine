575016d8236d48e4060df303be2cbb26a441a9f8
LJPayne98 <90472950+LJPayne98@users.noreply.github.com>
2023-10-16 21:56:56 +0100

    add premake5 deployment and removed pre generated files

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+worspace "Engine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include 
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	include 
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
